ea5e9849de4e4c936ccf2b20376638ebb366d5bc
Bima Sakti Nugroho <bima.27kd@yahoo.com>
2020-04-28 05:31:19 +0700

    Initial commit

---
+++ precache-manifest.468ffb5409780f9338fb1e605cd253f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3c33c907dfea62337945a6480a36e6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc5c2d2d55756be5add1",
+    "url": "/static/css/main.14249d99.chunk.css"
+  },
+  {
+    "revision": "1ce20b5b5e248f9f0204",
+    "url": "/static/js/2.a4dffacc.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.a4dffacc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc5c2d2d55756be5add1",
+    "url": "/static/js/main.92966ceb.chunk.js"
+  },
+  {
+    "revision": "dad4adfa7062cb4d7df7",
+    "url": "/static/js/runtime-main.597b7f10.js"
+  }
+]);
